795d8113f606c428a20b9e2e5e862f856c599ded
Shaurya Omm <shauryaomm@gmail.com>
2026-06-23 14:19:20 +0530

    fix: use explicit SMTP settings for Gmail

--- vmseva-backend/src/config/mailer.js
+++ vmseva-backend/src/config/mailer.js
@@ -2,14 +2,13 @@ const nodemailer = require('nodemailer');
 
 const sendMail = ({ to, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.MAIL_USER,
       pass: process.env.MAIL_PASS,
     },
-    connectionTimeout: 5000,
-    greetingTimeout: 5000,
-    socketTimeout: 10000,
   });
   return transporter.sendMail({ from: `"VMSeva" <${process.env.MAIL_USER}>`, to, subject, html });
 };
